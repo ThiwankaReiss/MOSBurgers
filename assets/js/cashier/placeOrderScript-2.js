fb83f7b54da5280560a4df7b1252b579143a796d
ThiwankaReiss <thiwankar2003@gmail.com>
2023-12-22 20:24:37 +0530

    Place order Table added

--- assets/js/cashier/placeOrderScript-2.js
+++ assets/js/cashier/placeOrderScript-2.js
@@ -21,9 +21,6 @@ customer.forEach(element => {
 });
 
 
-
-
-
 var num=0;
 
 function showSuggestions() {
@@ -82,20 +79,41 @@ function showALlSuggestions(){
     }
     
 }
-
+let itmCode;
 function setValues(suggestion){
     console.log(suggestion);
     item.forEach(element => {
         if(element.itemCode==suggestion || element.name==suggestion){
             document.getElementById('itmName').value=element.name;
+            document.getElementById('itmName').readOnly = true;
             document.getElementById('code').value=element.itemCode;
+            document.getElementById('code').readOnly = true;
             document.getElementById('unitPrice').value=element.price;
-            
+            document.getElementById('unitPrice').readOnly = true;
             document.getElementById('discount').value=element.discount;
-            custId=element.customerId;
+            document.getElementById('discount').readOnly = true;
+            itmCode=element.itemCode;
+            calculateAmount();
         }
     });
 }
+
+function calculateAmount(){
+    
+    if(itmCode!=null && itmCode!=""){
+
+        item.forEach(element => {
+            if (element.itemCode==itmCode) {
+                let qty=document.getElementById('qty').value;
+                console.log(qty);
+                document.getElementById('amount').value=element.price*qty*0.01*(100-element.discount);
+                document.getElementById('amount').readOnly = true;
+            }
+        });
+        
+    }
+}
+
 
 // Close suggestions when clicking outside the search bar
 window.addEventListener('click', (event) => {
@@ -107,6 +125,115 @@ window.addEventListener('click', (event) => {
         num=0;
     }
 });
+let orderId='O' + ('000' + order.length).slice(-3)
+document.getElementById('order-id-txt').innerHTML="Order ID : "+orderId;
+
+let details=[];
+function addTblRow(){
+    let tblData=`
+                <tr >
+                    <th>
+                        Code
+                    </th>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Unit Price
+                    </th>
+                    <th>
+                        Qty
+                    </th>
+                    <th>
+                        Discount
+                    </th>
+                    <th>
+                        Amount
+                    </th>
+                    <th>
+                        Delete
+                    </th>
+                    <th>
+                        Update
+                    </th>
+                </tr>
+                `
+    
+    
+    details.push(
+        new OrderDetail(
+            orderId,
+            document.getElementById('code').value,
+            document.getElementById('qty').value
+        )
+    );
+    
+    details.forEach(element => {
+        let itm1;
+        item.forEach(itm => {
+            if (itm.itemCode==element.code) {
+                itm1=itm;
+            }
+        });
+        tblData+=`
+    <tr>
+        <td>
+            ${itm1.itemCode}
+        </td>
+        <td>
+            ${itm1.name}
+        </td>
+        <td>
+            ${itm1.price}
+        </td>
+        <td>
+            ${element.qty}
+        </td>
+        <td>
+            ${itm1.discount}
+        </td>
+        <td>
+            ${itm1.price*element.qty*(100-itm1.discount)}
+        </td>
+        <td>
+            <button type="button" class="btn-background-1" onclick="deleteRow(this,'${itm1.code}')">Delete</button>
+        </td>
+        <td>
+            <button type="button" class="btn-background-1" onclick="generatePDF()">Update</button>
+        </td> 
+    </tr>                                                   
+    `
+    });
+
+    document.getElementById('tbl').innerHTML=tblData;
+
+}
+function deleteRow(button, customerId) {
+    // Traverse up the DOM to the closest 'tr' element and remove it
+    let row = button.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+    
+    customer.forEach(element => {
+        if(element.customerId==customerId){
+            customer.pop(element);
+        }     
+    });
+    localStorage.setItem('customerArray',JSON.stringify(customer));
+    
+    order.forEach(element => {
+        if(element.customerId==customerId){
+            orderDetail.forEach(detilOdr => {
+                if(element.orderId=detilOdr.orderDetail){
+                    detilOdr.pop();
+                }
+            });
+            order.pop(element);
+        }
+    });
+    localStorage.setItem('orderArray',JSON.stringify(order));
+    localStorage.setItem('orderDetailArray',JSON.stringify(orderDetail));
+
+}
 
 function openPage(){
      // Specify the path to the new HTML file
@@ -114,8 +241,6 @@ function openPage(){
         
      // Open the new HTML file in the same window
      window.location.href = newHTMLFilePath;
-
-
 }
 
 function openPlaceOrder3(){
@@ -125,7 +250,14 @@ function openPlaceOrder3(){
     // Open the new HTML file in the same window
     window.location.href = newHTMLFilePath;
 
+}
 
+class OrderDetail{
+    constructor(orderId,code,qty){
+        this.orderId=orderId;
+        this.code=code;
+        this.qty=qty;
+    }
 }
 localStorage.setItem('customerArray',JSON.stringify(customer));
 localStorage.setItem('itemArray',JSON.stringify(item));
